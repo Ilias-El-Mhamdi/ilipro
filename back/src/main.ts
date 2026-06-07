1a9d6b354b492a98fde0805f79ab496fc0cbcfb4
EL-MHAMDI Ilias <ilelmham@bouyguestelecom.fr>
2026-06-07 13:10:38 +0200

    Use environment variable for `FRONTEND_URL` in CORS configuration; add fallback to localhost URL.

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,8 +6,9 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
   app.use(cookieParser());
-  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
+  app.enableCors({ origin: process.env.FRONTEND_URL ?? 'http://localhost:5173', credentials: true });
   app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
