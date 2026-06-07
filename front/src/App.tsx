134aec973d88443158cff8f34c8ef99677491f9e
EL-MHAMDI Ilias <ilelmham@bouyguestelecom.fr>
2026-06-07 12:58:58 +0200

    Format code in `App.tsx` to improve readability and ensure consistency.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,22 @@
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 import './index.css'
 
+// commit to trigger pipeline - 1
+
 function App() {
-  const [message, setMessage] = useState<string>('')
+    const [message, setMessage] = useState<string>('')
 
-  useEffect(() => {
-    fetch('http://localhost:3000')
-      .then((res) => res.text())
-      .then(setMessage)
-  }, [])
+    useEffect(() => {
+        fetch('http://localhost:3000')
+            .then((res) => res.text())
+            .then(setMessage)
+    }, [])
 
-  return (
-    <div className="min-h-screen bg-gray-950 flex items-center justify-center">
-      <p className="text-2xl font-semibold text-white">{message}</p>
-    </div>
-  )
+    return (
+        <div className="min-h-screen bg-gray-950 flex items-center justify-center">
+            <p className="text-2xl font-semibold text-white">{message}</p>
+        </div>
+    )
 }
 
 export default App
